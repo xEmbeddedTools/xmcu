6d38fc31b9c815b5d59714dcd7075b93816166f0
kaczirosan <kaczirosan@gmail.com>
2024-07-15 09:56:43 +0200

    move systick and irq_config to ST/arm

--- soc/ST/arm/IRQ_config.hpp
+++ soc/ST/arm/IRQ_config.hpp
@@ -7,12 +7,10 @@
 
 namespace xmcu {
 namespace soc {
-namespace m0 {
 struct IRQ_config
 {
     std::uint32_t preempt_priority = 0;
-    std::uint32_t sub_priority     = 0;
+    std::uint32_t sub_priority = 0;
 };
-} // namespace m0
 } // namespace soc
 } // namespace xmcu
