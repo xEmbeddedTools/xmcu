d48ef593c3cdac29b2fb55a40f688d16e4973d69
Mateusz Semegen <mateusz.semegen@gmail.com>
2024-12-05 22:12:50 +0100

    wip

--- hal/config.hpp
+++ hal/config.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
-#if defined(STM32L010F4P6) || defined(STM32L010C6T6)
+/*
+ *  Copyright (c) xEmbeddedTools team and contributors.
+ *  Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for details.
+ */
+
+#if defined(XMCU_SOC_MODEL_STM32L010F4P6) || defined(XMCU_SOC_MODEL_STM32L010C6T6)
 #include <xmcu/soc/ST/arm/m0/l0/rm0451/config.hpp>
 #endif
 
-#if defined(STM32WB35CEU6A)
+#if defined(XMCU_SOC_MODEL_STM32WB35CEU6A)
 #include <xmcu/soc/ST/arm/m4/wb/rm0434/config.hpp>
 #endif
 
