c90624fc7b6d595cd7fa54455a5e076ccbde221a
Mateusz Semegen <mateusz.semegen@gmail.com>
2025-02-02 22:31:15 +0100

    Fixes

--- xmcu/config.hpp
+++ xmcu/config.hpp
@@ -6,10 +6,18 @@
  */
 
 #if defined(XMCU_SOC_MODEL_STM32L010F4P6) || defined(XMCU_SOC_MODEL_STM32L010C6T6)
+// CMSIS
+#include <stm32l0xx.h>
+
+// xmcu
 #include <soc/ST/arm/m0/l0/rm0451/config.hpp>
 #endif
 
 #if defined(XMCU_SOC_MODEL_STM32WB35CEU6A)
+//CMSIS
+#include <stm32wbxx.h>
+
+// xmcu
 #include <soc/ST/arm/m4/wb/rm0434/config.hpp>
 #endif
 
