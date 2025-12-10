e48d2aa76c8b909c109dbdf7d1ac4bd9030b559b
Krzysiek Borowy <Krzysztof.Borowy@heavykinematic.com>
2025-12-10 13:23:13 +0100

    fix ifdef

--- xmcu/hal/peripherals/wwdg.hpp
+++ xmcu/hal/peripherals/wwdg.hpp
@@ -5,10 +5,10 @@
  *  Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for details.
  */
 
- #if defined(STM32WB)
 // xmcu
 #include <xmcu/config.hpp>
 
+#if defined(STM32WB)
 // clang-format off
 // soc
 #include DECORATE_INCLUDE_PATH(soc/XMCU_SOC_VENDOR/XMCU_SOC_ARCHITECTURE/XMCU_SOC_CORE_FAMILY/XMCU_SOC_VENDOR_FAMILY/XMCU_SOC_VENDOR_FAMILY_RM/peripherals/wwdg/wwdg.hpp)
@@ -18,4 +18,6 @@ namespace xmcu::hal::peripherals {
 using wwdg = xmcu::soc::XMCU_SOC_VENDOR::XMCU_SOC_ARCHITECTURE::XMCU_SOC_CORE_FAMILY::XMCU_SOC_VENDOR_FAMILY::
     XMCU_SOC_VENDOR_FAMILY_RM::peripherals::wwdg;
 } // namespace xmcu::hal::peripherals
+#else
+#error "Unsupported platform"
 #endif
